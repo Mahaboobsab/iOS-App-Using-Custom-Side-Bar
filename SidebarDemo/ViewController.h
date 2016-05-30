202a811f09ecdaa0481dbce0462b5f630af4c63c
Mahaboobsab Nadaf <mrnadaf2010@gmail.com>
2016-05-30 13:51:38 +0530

    Initial Commit

--- SidebarDemo/ViewController.h
+++ SidebarDemo/ViewController.h
@@ -3,7 +3,7 @@
 //  SidebarDemo
 //
 //  Created by Mahaboobsab Nadaf on 25/05/16.
-//  Copyright © 2016 com.NeoRays. All rights reserved.
+//  Copyright © 2016 com.NeoRays. All rights reserved
 //
 
 #import <UIKit/UIKit.h>
